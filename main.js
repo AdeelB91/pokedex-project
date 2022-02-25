65f541c87c8cd1260285688ae3b309ddd55afdcc
Adeel Butt <adeel.butt.hh@gmail.com>
2022-02-25 11:24:48 +0100

    added img

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import './style.css'
+import './style.css';
+
+const pokemonName = 'Pikachu';
+const header = '<img alt="Pokedex Header" src="./Images/Pokedex-font.png">';
 
 document.querySelector('#app').innerHTML = `
+<header class=center >${header}</header>
+<p>Hi It's me ${pokemonName}!</p>
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
